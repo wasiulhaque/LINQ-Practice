17d81778733a515e375405f3dd4b73b9dd046f08
Wasiul Haque <wasiulhq@gmail.com>
2022-06-05 14:03:17 +0600

    Basic LINQ syntax from https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/working-with-linq

--- BasicLINQ/Program.cs
+++ BasicLINQ/Program.cs
@@ -1,19 +1,48 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace BasicLINQ
 {
     public class Program
     {
+        static IEnumerable<string> Suits()
+        {
+            yield return "clubs";
+            yield return "diamonds";
+            yield return "hearts";
+            yield return "spades";
+        }
+
+        static IEnumerable<string> Ranks()
+        {
+            yield return "two";
+            yield return "three";
+            yield return "four";
+            yield return "five";
+            yield return "six";
+            yield return "seven";
+            yield return "eight";
+            yield return "nine";
+            yield return "ten";
+            yield return "jack";
+            yield return "queen";
+            yield return "king";
+            yield return "ace";
+        }
+        
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var startingDeck = from s in Suits()
+                from r in Ranks()
+                select new { Suit = s, Rank = r };
+
+            foreach (var card in startingDeck)
+            {
+                Console.WriteLine(card);
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
